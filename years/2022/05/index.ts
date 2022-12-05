1ec71c0a2327bbf17a869c418131c0c67aab4a83
Jakob Rubin <647846+Grygon@users.noreply.github.com>
2022-12-05 00:34:22 -0600

    Day 5

--- years/2022/05/index.ts
+++ years/2022/05/index.ts
@@ -4,6 +4,7 @@ import * as test from "../../../util/test";
 import chalk from "chalk";
 import { log, logSolution, trace } from "../../../util/log";
 import { performance } from "perf_hooks";
+import { buffer } from "stream/consumers";
 
 const YEAR = 2022;
 const DAY = 5;
@@ -13,16 +14,110 @@ const DAY = 5;
 // problem url  : https://adventofcode.com/2022/day/5
 
 async function p2022day5_part1(input: string, ...params: any[]) {
-	return "Not implemented";
+	const lines = input.split("\n");
+	let state: string[][] = [[],[],[],[],[],[],[],[],[]];
+	let instructions: {count:number,from:number,to:number}[] = [];
+	for (const line of lines) {
+		if(line.substring(0,1) == "m") {
+			let extract = line.match(/\d+/g);
+			if(!extract || extract.length < 3) throw Error("Bad extract");
+			instructions.push({
+				count: parseInt(extract[0]),
+				from: parseInt(extract[1]),
+				to: parseInt(extract[2])
+			});
+		} else {
+			for (let i = 0; i < 9; i++) {
+				let crate = line.substring(i*4+1,i*4+2);
+				if(parseInt(crate)) continue;
+				if(crate.length > 0 && crate!==" ") state[i].push(crate);				
+			}
+		}
+	}
+
+	state = state.map((s) => s.reverse());
+
+	instructions.forEach(command => {
+		for (let i = 0; i < command.count; i++) {
+			let from = state[command.from - 1].pop();
+			if(!from) throw Error("Empty pile");
+			state[command.to - 1].push(from);
+		}
+	});
+
+	let final = "";
+
+	state.forEach(s => {
+		final = final.concat(s[s.length - 1] ? s[s.length - 1] : "");
+	});
+
+	return final;
 }
 
 async function p2022day5_part2(input: string, ...params: any[]) {
-	return "Not implemented";
+	const lines = input.split("\n");
+	let state: string[][] = [[],[],[],[],[],[],[],[],[]];
+	let instructions: {count:number,from:number,to:number}[] = [];
+	for (const line of lines) {
+		if(line.substring(0,1) == "m") {
+			let extract = line.match(/\d+/g);
+			if(!extract || extract.length < 3) throw Error("Bad extract");
+			instructions.push({
+				count: parseInt(extract[0]),
+				from: parseInt(extract[1]),
+				to: parseInt(extract[2])
+			});
+		} else {
+			for (let i = 0; i < 9; i++) {
+				let crate = line.substring(i*4+1,i*4+2);
+				if(parseInt(crate)) continue;
+				if(crate.length > 0 && crate!==" ") state[i].push(crate);				
+			}
+		}
+	}
+
+	state = state.map((s) => s.reverse());
+
+	instructions.forEach(command => {
+		let from = state[command.from - 1].splice(
+			state[command.from - 1].length - command.count,
+			command.count);
+		if(!from) throw Error("Empty pile");
+		state[command.to - 1] = state[command.to - 1].concat(from);
+	});
+
+	let final = "";
+
+	state.forEach(s => {
+		final = final.concat(s[s.length - 1] ? s[s.length - 1] : "");
+	});
+
+	return final;
 }
 
 async function run() {
-	const part1tests: TestCase[] = [];
-	const part2tests: TestCase[] = [];
+	const part1tests: TestCase[] = [{
+		input: `    [D]    
+[N] [C]    
+[Z] [M] [P]
+ 1   2   3 
+
+move 1 from 2 to 1
+move 3 from 1 to 3
+move 2 from 2 to 1
+move 1 from 1 to 2`, expected: "CMZ"
+	}];
+	const part2tests: TestCase[] = [{
+		input: `    [D]    
+[N] [C]    
+[Z] [M] [P]
+ 1   2   3 
+
+move 1 from 2 to 1
+move 3 from 1 to 3
+move 2 from 2 to 1
+move 1 from 1 to 2`, expected: "MCD"
+	}];
 
 	// Run tests
 	test.beginTests();
