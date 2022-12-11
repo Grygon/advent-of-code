a877ee803a2db47484e285489a324a87bb8d5b6c
Jakob Rubin <647846+Grygon@users.noreply.github.com>
2022-12-10 18:34:42 -0600

    Day 10

--- years/2022/10/index.ts
+++ years/2022/10/index.ts
@@ -4,6 +4,7 @@ import * as test from "../../../util/test";
 import chalk from "chalk";
 import { log, logSolution, trace } from "../../../util/log";
 import { performance } from "perf_hooks";
+import { buffer } from 'stream/consumers';
 
 const YEAR = 2022;
 const DAY = 10;
@@ -13,16 +14,374 @@ const DAY = 10;
 // problem url  : https://adventofcode.com/2022/day/10
 
 async function p2022day10_part1(input: string, ...params: any[]) {
-	return "Not implemented";
+	const lines = input.split("\n");
+	let commands = [];
+	let x = 1;
+	let sum = 0;
+	for (const line of lines) {
+		let command = line.split(" ");
+
+		if(command[0] == "noop") {
+			commands.push(0);
+		} else if(command[0] == "addx") {
+			commands.push(0, parseInt(command[1]));
+		}
+	}
+
+	commands.forEach((command, i) => {
+		// Inspect during cycle
+		if((i + 1) % 40 == 20) {
+			sum += x * (i + 1);
+		}
+
+		// End cycle
+		x += command;
+	})
+
+	return sum;
 }
 
 async function p2022day10_part2(input: string, ...params: any[]) {
-	return "Not implemented";
+	const lines = input.split("\n");
+	let commands = [];
+	let x = 1;
+	let width = 40;
+	let screen = "\n";
+	for (const line of lines) {
+		let command = line.split(" ");
+
+		if(command[0] == "noop") {
+			commands.push(0);
+		} else if(command[0] == "addx") {
+			commands.push(0, parseInt(command[1]));
+		}
+	}
+
+	commands.forEach((command, i) => {
+		let horizPos = i % width;
+
+		// Inspect during cycle
+		if(Math.abs(horizPos - x) <= 1) {
+			screen = screen.concat("#");
+		} else {
+			screen = screen.concat(".");
+		}
+
+		if(horizPos == width - 1) {
+			screen = screen.concat("\n")
+		}
+
+		// End cycle
+		x += command;
+	})
+
+	return screen;
 }
 
 async function run() {
-	const part1tests: TestCase[] = [];
-	const part2tests: TestCase[] = [];
+	const part1tests: TestCase[] = [
+		{input: `addx 15
+addx -11
+addx 6
+addx -3
+addx 5
+addx -1
+addx -8
+addx 13
+addx 4
+noop
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx -35
+addx 1
+addx 24
+addx -19
+addx 1
+addx 16
+addx -11
+noop
+noop
+addx 21
+addx -15
+noop
+noop
+addx -3
+addx 9
+addx 1
+addx -3
+addx 8
+addx 1
+addx 5
+noop
+noop
+noop
+noop
+noop
+addx -36
+noop
+addx 1
+addx 7
+noop
+noop
+noop
+addx 2
+addx 6
+noop
+noop
+noop
+noop
+noop
+addx 1
+noop
+noop
+addx 7
+addx 1
+noop
+addx -13
+addx 13
+addx 7
+noop
+addx 1
+addx -33
+noop
+noop
+noop
+addx 2
+noop
+noop
+noop
+addx 8
+noop
+addx -1
+addx 2
+addx 1
+noop
+addx 17
+addx -9
+addx 1
+addx 1
+addx -3
+addx 11
+noop
+noop
+addx 1
+noop
+addx 1
+noop
+noop
+addx -13
+addx -19
+addx 1
+addx 3
+addx 26
+addx -30
+addx 12
+addx -1
+addx 3
+addx 1
+noop
+noop
+noop
+addx -9
+addx 18
+addx 1
+addx 2
+noop
+noop
+addx 9
+noop
+noop
+noop
+addx -1
+addx 2
+addx -37
+addx 1
+addx 3
+noop
+addx 15
+addx -21
+addx 22
+addx -6
+addx 1
+noop
+addx 2
+addx 1
+noop
+addx -10
+noop
+noop
+addx 20
+addx 1
+addx 2
+addx 2
+addx -6
+addx -11
+noop
+noop
+noop`, 
+	expected: '13140'}
+	];
+	const part2tests: TestCase[] = [
+		{input: `addx 15
+addx -11
+addx 6
+addx -3
+addx 5
+addx -1
+addx -8
+addx 13
+addx 4
+noop
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx -35
+addx 1
+addx 24
+addx -19
+addx 1
+addx 16
+addx -11
+noop
+noop
+addx 21
+addx -15
+noop
+noop
+addx -3
+addx 9
+addx 1
+addx -3
+addx 8
+addx 1
+addx 5
+noop
+noop
+noop
+noop
+noop
+addx -36
+noop
+addx 1
+addx 7
+noop
+noop
+noop
+addx 2
+addx 6
+noop
+noop
+noop
+noop
+noop
+addx 1
+noop
+noop
+addx 7
+addx 1
+noop
+addx -13
+addx 13
+addx 7
+noop
+addx 1
+addx -33
+noop
+noop
+noop
+addx 2
+noop
+noop
+noop
+addx 8
+noop
+addx -1
+addx 2
+addx 1
+noop
+addx 17
+addx -9
+addx 1
+addx 1
+addx -3
+addx 11
+noop
+noop
+addx 1
+noop
+addx 1
+noop
+noop
+addx -13
+addx -19
+addx 1
+addx 3
+addx 26
+addx -30
+addx 12
+addx -1
+addx 3
+addx 1
+noop
+noop
+noop
+addx -9
+addx 18
+addx 1
+addx 2
+noop
+noop
+addx 9
+noop
+noop
+noop
+addx -1
+addx 2
+addx -37
+addx 1
+addx 3
+noop
+addx 15
+addx -21
+addx 22
+addx -6
+addx 1
+noop
+addx 2
+addx 1
+noop
+addx -10
+noop
+noop
+addx 20
+addx 1
+addx 2
+addx 2
+addx -6
+addx -11
+noop
+noop
+noop`, 
+	expected: `
+##..##..##..##..##..##..##..##..##..##..
+###...###...###...###...###...###...###.
+####....####....####....####....####....
+#####.....#####.....#####.....#####.....
+######......######......######......####
+#######.......#######.......#######.....`}];
 
 	// Run tests
 	test.beginTests();
