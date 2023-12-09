fdfd5e0cff49ec4444db6a88f47907a324508c3e
Jakob Rubin <647846+Grygon@users.noreply.github.com>
2023-12-08 21:35:10 -0600

    Day 8 here we goooo

--- years/2023/08/index.ts
+++ years/2023/08/index.ts
@@ -3,6 +3,7 @@ import * as test from "../../../util/test";
 import chalk from "chalk";
 import { log, logSolution, trace } from "../../../util/log";
 import { performance } from "perf_hooks";
+import { sum } from "lodash";
 
 const YEAR = 2023;
 const DAY = 8;
@@ -12,16 +13,97 @@ const DAY = 8;
 // problem url  : https://adventofcode.com/2023/day/8
 
 async function p2023day8_part1(input: string, ...params: any[]) {
-	return "Not implemented";
+	let instruction = input.split("\n\n")[0];
+	let rawNodes = input.split("\n\n")[1].split('\n');
+	let nodes = new Map<string, string[]>();
+	let counter = 0;
+	let curNode = "AAA";
+
+	rawNodes.forEach(node => {
+		let match = node.match(/(\D+) \= \((\D+)\, (\D+)\)/);
+		if(!match) throw Error("Error")
+		nodes.set(match[1], [match[2], match[3]])
+	});
+
+	while(curNode !== "ZZZ") {
+		if(instruction.slice(0,1) == "L") {
+			let procNode = nodes.get(curNode);
+			if(!procNode) throw Error("Error")
+			curNode = procNode[0]
+		} else {
+			let procNode = nodes.get(curNode);
+			if(!procNode) throw Error("Error")
+			curNode = procNode[1]
+		}
+		counter++;
+		instruction = instruction.slice(1) + instruction.slice(0,1);
+	}
+
+	return counter;
 }
 
 async function p2023day8_part2(input: string, ...params: any[]) {
-	return "Not implemented";
+	let instruction = input.split("\n\n")[0];
+	let rawNodes = input.split("\n\n")[1].split('\n');
+	let nodes = new Map<string, string[]>();
+	let startingNodes: string[] = [];
+
+	rawNodes.forEach(node => {
+		let match = node.match(/(\w+) \= \((\w+)\, (\w+)\)/);
+		if(!match) throw Error("Error")
+		nodes.set(match[1], [match[2], match[3]])
+		if(match[1][2] == "A") startingNodes.push(match[1])
+	});
+
+	let allRuns = startingNodes.map(startNode => {
+		let counter = 0;
+		let curNode = startNode;
+		while(curNode[2] !== "Z") {
+			if(instruction.slice(0,1) == "L") {
+				let procNode = nodes.get(curNode);
+				if(!procNode) throw Error("Error, no L node")
+				curNode = procNode[0]
+			} else {
+				let procNode = nodes.get(curNode);
+				if(!procNode) throw Error("Error, no R node")
+				curNode = procNode[1]
+			}
+			counter++;
+			instruction = instruction.slice(1) + instruction.slice(0,1);
+		}
+		return counter;
+	});
+
+	const gcd: (a: number, b: number) => number = (a: number, b: number) => {return b == 0 ? a : gcd (b, a % b)}
+	const lcm = (a: number, b: number) =>  a / gcd (a, b) * b
+
+	return allRuns.reduce(lcm);
 }
 
 async function run() {
-	const part1tests: TestCase[] = [];
-	const part2tests: TestCase[] = [];
+	const part1tests: TestCase[] = [{input:`RL
+
+AAA = (BBB, CCC)
+BBB = (DDD, EEE)
+CCC = (ZZZ, GGG)
+DDD = (DDD, DDD)
+EEE = (EEE, EEE)
+GGG = (GGG, GGG)
+ZZZ = (ZZZ, ZZZ)`, expected: '2'},{input:`LLR
+
+AAA = (BBB, BBB)
+BBB = (AAA, ZZZ)
+ZZZ = (ZZZ, ZZZ)`, expected: '6'}];
+	const part2tests: TestCase[] = [{input:`LR
+
+11A = (11B, XXX)
+11B = (XXX, 11Z)
+11Z = (11B, XXX)
+22A = (22B, XXX)
+22B = (22C, 22C)
+22C = (22Z, 22Z)
+22Z = (22B, 22B)
+XXX = (XXX, XXX)`, expected: '6'}];
 
 	// Run tests
 	test.beginTests();
