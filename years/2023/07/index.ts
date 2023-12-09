0016db4d25281ad8f8c1b133ff2a8fdcd8b92bfa
Jakob Rubin <647846+Grygon@users.noreply.github.com>
2023-12-08 20:48:39 -0600

    Better nate than lever

--- years/2023/07/index.ts
+++ years/2023/07/index.ts
@@ -11,17 +11,149 @@ const DAY = 7;
 // data path    : C:\Users\Grygon\Documents\advent-of-code\years\2023\07\data.txt
 // problem url  : https://adventofcode.com/2023/day/7
 
+interface Hand {
+	hand: string, 
+	bid: number
+}
+
+let valMappings = {
+	'2': 2,
+	'3': 3,
+	'4': 4,
+	'5': 5,
+	'6': 6,
+	'7': 7,
+	'8': 8,
+	'9': 9,
+	'T': 10,
+	'J': 1,
+	'Q': 12,
+	'K': 13,
+	'A': 14
+}
+
 async function p2023day7_part1(input: string, ...params: any[]) {
-	return "Not implemented";
+	let lines: string[] = input.split('\n');
+	let hands: Hand[] = lines.map((i) => {return {hand: i.split(' ')[0], bid: +i.split(' ')[1]}})
+	let rankedHands: Hand[][] = [[],[],[],[],[],[],[]]
+
+	function returnType(hand: string): 0|1|2|3|4|5|6 {
+		let valCounter: number[] = []
+		Object.keys(valMappings).forEach((val) => {
+			valCounter.push([...hand.matchAll(new RegExp(val,"g"))].length)
+		})
+		valCounter.sort((a, b) => b - a);
+		if(valCounter[0] == 5) return 0;
+		if(valCounter[0] == 4) return 1;
+		if(valCounter[0] == 3 && valCounter[1] == 2) return 2;
+		if(valCounter[0] == 3) return 3;
+		if(valCounter[0] == 2 && valCounter[1] == 2) return 4;
+		if(valCounter[0] == 2) return 5;
+		return 6
+	}
+
+	hands.forEach((hand) => {
+		rankedHands[returnType(hand.hand)].push(hand)
+	})
+
+	rankedHands.forEach((ranking) => {
+		ranking.sort((hand1, hand2) => {
+			for (let i = 0; i < hand1.hand.length; i++) {
+				const char1 = hand1.hand[i];
+				const char2 = hand2.hand[i];
+				//@ts-ignore
+				if(char1 !== char2) return valMappings[char2] - valMappings[char1]
+			}
+			return 0;
+		})
+	})
+	
+	let flatHands = rankedHands.flat().reverse();
+	let sum = 0;
+
+	flatHands.forEach((hand, i) => {
+		sum += hand.bid * (i + 1);
+	})
+	
+	return sum;
 }
 
 async function p2023day7_part2(input: string, ...params: any[]) {
-	return "Not implemented";
+	let lines: string[] = input.split('\n');
+	let hands: Hand[] = lines.map((i) => {return {hand: i.split(' ')[0], bid: +i.split(' ')[1]}})
+	let rankedHands: Hand[][] = [[],[],[],[],[],[],[]]
+
+	function returnType(hand: string): 0|1|2|3|4|5|6 {
+		let valCounter: number[] = []
+		let jokerCounter = 0;
+		Object.keys(valMappings).forEach((val) => {
+			if(val=='J') {
+				jokerCounter = [...hand.matchAll(new RegExp(val,"g"))].length;
+			} else {
+				valCounter.push([...hand.matchAll(new RegExp(val,"g"))].length)
+			}
+		})
+		valCounter.sort((a, b) => b - a);
+		if(valCounter[0] + jokerCounter == 5) return 0;
+		if(valCounter[0] + jokerCounter == 4) return 1;
+		if(valCounter[0] + jokerCounter == 3 && valCounter[1] == 2) return 2;
+		if(valCounter[0] + jokerCounter == 3) return 3;
+		if(valCounter[0] + jokerCounter == 2 && valCounter[1] == 2) return 4;
+		if(valCounter[0] + jokerCounter == 2) return 5;
+		return 6
+	}
+
+	hands.forEach((hand) => {
+		rankedHands[returnType(hand.hand)].push(hand)
+	})
+
+	rankedHands.forEach((ranking) => {
+		ranking.sort((hand1, hand2) => {
+			for (let i = 0; i < hand1.hand.length; i++) {
+				const char1 = hand1.hand[i];
+				const char2 = hand2.hand[i];
+				//@ts-ignore
+				if(char1 !== char2) return valMappings[char2] - valMappings[char1]
+			}
+			return 0;
+		})
+	})
+	
+	let flatHands = rankedHands.flat().reverse();
+	let sum = 0;
+
+	flatHands.forEach((hand, i) => {
+		sum += hand.bid * (i + 1);
+	})
+	
+	return sum;
 }
 
 async function run() {
-	const part1tests: TestCase[] = [];
-	const part2tests: TestCase[] = [];
+	const part1tests: TestCase[] = [{input: `32T3K 765
+T55J5 684
+KK677 28
+KTJJT 220
+QQQJA 483`, expected: '6440'},
+{input: `AAAAA 2
+22222 3
+AAAAK 5
+22223 7
+AAAKK 11
+22233 13
+AAAKQ 17
+22234 19
+AAKKQ 23
+22334 29
+AAKQJ 31
+22345 37
+AKQJT 41
+23456 43`, expected: "1343"}];
+	const part2tests: TestCase[] = [{input: `32T3K 765
+T55J5 684
+KK677 28
+KTJJT 220
+QQQJA 483`, expected: '5905'}];
 
 	// Run tests
 	test.beginTests();
